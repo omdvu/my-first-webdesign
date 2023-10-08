8d5f4f1f1593541dd5c3919d444b9653ad2ff500
omdvu <147306721+omdvu@users.noreply.github.com>
2023-10-08 14:15:22 +0530

    Add files via upload

---
+++ apple.js
@@ -0,0 +1,9 @@
+function ttl(){
+	const x = document.getElementById("iphone").value;
+	const y = document.getElementById("acc").value;
+	const z=Number(x)+Number(y);
+	document.getElementById("total").innerHTML = "$"+z;
+}
+function output(){
+	var x = document.getElementById("name").value;
+}
